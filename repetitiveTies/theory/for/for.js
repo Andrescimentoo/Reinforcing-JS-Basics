04c74ad184b289ea610145db418d5ff07bef8a1b
Andre Nascimento <1914andre@gmail.com>
2025-02-04 19:11:36 -0300

    feat: new structuring to the pasts

---
+++ repetitiveTies/theory/for/for.js
@@ -0,0 +1,13 @@
+//ponto inicial, condiçao, incremento
+
+const arrayNumeros = [0,1,2,3,4,5,6]
+
+for (let index = 0;   index < arrayNumeros.length;  index++) {
+    const element = arrayNumeros[index];
+    console.log(element)
+} // O laço vai continuar enquanto o índice (0,1,2....) for menor que o comprimento do array  
+
+
+
+
+
