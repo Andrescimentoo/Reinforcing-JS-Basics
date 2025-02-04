82cce28a9cb0091fa09b4d94c20e3e84e5c5b01f
Andre Nascimento <1914andre@gmail.com>
2025-02-04 19:00:21 -0300

    feat: adding structure to the doWhile

---
+++ repetitiveTies/doWile/doWile.js
@@ -0,0 +1,10 @@
+
+
+let inscrito = "sim"
+
+do { 
+    prompt("increva-se para continuar")
+    inscrito = prompt("voce ja e inscrito no canal?")
+} while (inscrito !== "sim") //executa ate que isso, seja falso, verdadeiro seria tudo o que nao corresponde a variavel inscrito 
+
+
