a48e8a204942433e33c75df9751835e8ce63fa0f
Andre Nascimento <1914andre@gmail.com>
2025-02-04 19:04:29 -0300

    feat: adding structure to sistem registrationPage

---
+++ javaScript-prototypes/loginPage/registrationPage.js
@@ -0,0 +1,28 @@
+// se o usuario colocar nos campos gmail e senha os dados correspondentes desse array, enviar um Sucesso
+
+// se o usuraio digitar um nome e uma senha ou emviar incompletos os campos, mostrar erro 
+
+
+//como construir isso? com as tecnologias que eu conheço
+const usersCadastrados = [
+    
+    { gmail:"Messi",
+      senha:"Barcelona"
+    },
+    
+    { gmail:"Suarez",
+      senha:"Barcelona"
+    },
+    
+    { gmail:"Neymar",
+      senha: "Barcelona"
+    }
+]
+
+const inputGmail = document.getElementById("input-gmail")
+const inputPassword = document.getElementById("input-senha") // se nesses 2 inputs o usuario colocar os valores ddo array , top se nao, erro
+
+const validacao = () => {
+    
+  
+}
