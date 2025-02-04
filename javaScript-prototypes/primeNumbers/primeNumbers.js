b5f32f4a6a13bbc15d3b9a0dcd264c3712091f45
Andre Nascimento <1914andre@gmail.com>
2025-02-04 19:02:21 -0300

    feat: adding structure to sistem primeNumbers

---
+++ javaScript-prototypes/primeNumbers/primeNumbers.js
@@ -0,0 +1,13 @@
+//primeiro de tudo vou ter que fazer verificar quais numeros sao divisiveis por eles mesmos 
+const arrayNumbers = [0,1,2,3,4,5,6,7,8,9,10]
+
+
+const primeNumbers = () => {
+     for (let index = 0;  index < arrayNumbers.length;  index++) {
+        const numbers = arrayNumbers[index];
+        console.log(numbers)
+      
+
+ }    }
+
+
