4b44aa79405a138074158b20ea20527e2ac862b3
Andre Nascimento <1914andre@gmail.com>
2025-02-04 19:01:24 -0300

    feat: adding structure to wile

---
+++ repetitiveTies/wile/wile.js
@@ -0,0 +1,12 @@
+// wile depende de uma variavel externa, fora do bloco de repetiçao 
+// Em termos leigos se o resultado nao for = a simm continua executando
+
+
+let inscrito = "";
+
+while (inscrito !== "sim") {
+    inscrito = prompt("voce ja e inscrito no canal?")
+}
+
+//se nao for isso daqui,   entao vou continuar execurtando ate que seja isso daqui
+
